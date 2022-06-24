59ea5a8e2a479d412c47199a12610f3945fec119
hasanrazahabi <hasanrazahabi@gmail.com>
2022-06-24 10:37:44 +0500

    worked on card items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+  const filterItems = (category) => {
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+  return (
+    <main>
+      <section className="menu section">
+        <h2
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginBottom: "20px",
+          }}
+        >
+          Our Menu
+        </h2>
+        <div className="underline" style={{ marginBottom: "20px" }}></div>
+        <Categories filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
